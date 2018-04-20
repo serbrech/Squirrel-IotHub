76567f454db6d7a3a9953d1c8b6350bd220f0cc0
Daniele Antonio Maggio <damaggio@microsoft.com>
2018-04-20 12:53:34 +0200

    Added IoT Hub integration

--- src/SquirrelConsole/Program.cs
+++ src/SquirrelConsole/Program.cs
@@ -4,28 +4,77 @@ using System.Configuration;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace SquirrelConsole
 {
+    class updateAppPayload
+    {
+        public string ReleaseLocation { get; set; }
+        public string NewVersion { get; set; }
+    }
     class Program
     {
-        static CancellationToken updateCancellationToken = new CancellationToken();
-        //Change the release location to your local Releases folder, or to a url you control!
-        static string releaseLocation = ConfigurationManager.AppSettings["releaselocation"];
+        //Change the deviceConnectionString to a proper Connection String
+        static string deviceConnectionString = ConfigurationManager.AppSettings["deviceconnectionstring"];
+        static DeviceClient Client = null;
+
+
         static void Main(string[] args)
         {
-            //This can be used to inform about update status
-            SquirrelAwareApp.HandleEvents(onAppUpdate: v => Console.WriteLine($"updating to {v}"));
+            try
+            {
+                //This can be used to inform about update status
+                SquirrelAwareApp.HandleEvents(onAppUpdate: v => Console.WriteLine($"updating to {v}"));
 
-            //We run this in the background. do not await. Not using Timer to avoid overlapping runs. I want a sliding interval.
-            var notAwaitedTask = RunPeriodicAsync(() => UpdateSimple(releaseLocation), TimeSpan.Zero, TimeSpan.FromSeconds(5), updateCancellationToken);
-            if (notAwaitedTask.Status == TaskStatus.Created)
-                notAwaitedTask.Start();
+                Console.WriteLine("Current version is : " + Assembly.GetExecutingAssembly().GetName().Version);
 
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Current version is : " + Assembly.GetExecutingAssembly().GetName().Version);
-            Thread.Sleep(Timeout.Infinite);
+                Console.WriteLine("Connecting to IoT Hub");
+                Client = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+
+                // Set callback for "updateApp" method
+                Client.SetMethodHandlerAsync("updateApp", updateAppMethod, null).Wait();
+                Console.WriteLine("Waiting for direct method call\n Press enter to exit.");
+                Console.ReadLine();
+
+                Console.WriteLine("Exiting...");
+
+                // Remove the "updateApp" handler
+                Client.SetMethodHandlerAsync("updateApp", null, null).Wait();
+                Client.CloseAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
         }
+
+
+        static async Task<MethodResponse> updateAppMethod(MethodRequest methodRequest, object userContext)
+        {
+            updateAppPayload payload;
+            string result;
+            try
+            {
+                payload = JsonConvert.DeserializeObject<updateAppPayload>(methodRequest.DataAsJson);
+
+                result = "{\"Status\":\"Started update to new version}\"}";
+                new Thread(() =>
+                {
+                    Thread.CurrentThread.IsBackground = true;
+                    UpdateSimple(payload.ReleaseLocation);
+                }).Start();
+                return new MethodResponse(Encoding.UTF8.GetBytes(result), 200);
+            }
+            catch (Exception ex)
+            {
+                return new MethodResponse(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(ex.Message)), 500);
+            }
+        }
+
 
         private static void UpdateSimple(string releaseLocation)
         {
@@ -76,30 +125,5 @@ namespace SquirrelConsole
                 }
             }
         }
-
-        // The `onTick` method will be called periodically unless cancelled.
-        private static async Task RunPeriodicAsync(Action onTick,
-                                                   TimeSpan dueTime,
-                                                   TimeSpan interval,
-                                                   CancellationToken token)
-        {
-            // Initial wait time before we begin the periodic loop.
-            if (dueTime > TimeSpan.Zero)
-                await Task.Delay(dueTime, token);
-
-            // Repeat this loop until cancelled.
-            while (!token.IsCancellationRequested)
-            {
-                // Call our onTick function.
-                onTick?.Invoke();
-
-                // Wait to repeat again.
-                if (interval > TimeSpan.Zero)
-                    await Task.Delay(interval, token);
-            }
-        }
-
     }
-
-
 }
